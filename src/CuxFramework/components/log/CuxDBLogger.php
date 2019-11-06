72873b713bc0f36df3dd03ff11ebfe410c18a898
Mihail Cuculici <mihail.cuculici@bursatransport.com>
2019-11-06 14:25:04 +0200

    DBLogger fix

--- src/CuxFramework/components/log/CuxDBLogger.php
+++ src/CuxFramework/components/log/CuxDBLogger.php
@@ -21,17 +21,16 @@ class CuxDBLogger extends CuxLogger {
             $micro = sprintf("%06d", ($t - floor($t)) * 1000000);
             $d = new \DateTime(date('Y-m-d H:i:s.' . $micro, $t));
             
-//            $tableName = Cux::getInstance()->{$this->db}->quoteTableName($this->logTable);
-//            $sql = "INSERT INTO $tableName (log_time, level, message, context)
-//                    VALUES (:log_time, :level, :message, :context)";
-//            $stmt = Cux::getInstance()->{$this->db}->prepare($sql);
-//            $stmt->bindValue(":log_time", $d->format("Y-m-d H:i:s") );
-//            $stmt->bindValue(":level", $level);
-//            $stmt->bindValue(":message", $message );
-//            $stmt->bindValue(":context", json_encode($context) );
-//            
-//            return $stmt->execute();
-            return true;
+            $tableName = Cux::getInstance()->{$this->db}->quoteTableName($this->logTable);
+            $sql = "INSERT INTO $tableName (log_time, level, message, context)
+                    VALUES (:log_time, :level, :message, :context)";
+            $stmt = Cux::getInstance()->{$this->db}->prepare($sql);
+            $stmt->bindValue(":log_time", $d->format("Y-m-d H:i:s") );
+            $stmt->bindValue(":level", $level);
+            $stmt->bindValue(":message", $message );
+            $stmt->bindValue(":context", json_encode($context) );
+            
+            return $stmt->execute();
         }
         return false;
     }
